f3be6d64a4b72f1524d640f703aee227308a83bc
dhia hamedi <d.hamdi1710@myu.universitecentrale.tn^C>
2025-06-21 20:10:19 +0100

    Modification sur le hero

--- app/Components/Hero/Hero.tsx
+++ app/Components/Hero/Hero.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 import Style from './Hero.module.css'
 
 function Hero() {
   return (
     <>
       <Head>
-        <link
-          rel="preload"
-          as="image"
-          href="/img/Hero/inner-page1.webp"
-        />
+        <link rel="preload" as="image" href="/img/Hero/inner-page1.webp" />
       </Head>
 
       <div className={Style.hero}>
+        <div className={Style.imageWrapper}>
+          <Image src="/img/Hero/inner-page1.webp" alt="Hero background"
+            fill style={{ objectFit: 'cover' }} placeholder="blur"
+            blurDataURL="/img/Hero/inner-page1-blur.webp"  priority />
+        </div>
+
         <div className={Style['hero-content']} data-aos="fade-up">
           <h1>مرحباً بكم في ترانسيسكو لوجستيك</h1>
           <div className={Style.breadcrumb}>
