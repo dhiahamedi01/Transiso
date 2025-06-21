b8f8cc202ef0e22b960892b342ebfab3926ee5f1
dhia hamedi <d.hamdi1710@myu.universitecentrale.tn^C>
2025-06-21 20:04:42 +0100

    Modification sur le hero

--- app/Components/Hero/Hero.tsx
+++ app/Components/Hero/Hero.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import Head from 'next/head'
 import Style from './Hero.module.css'
 
 function Hero() {
   return (
-    <div className={Style.hero}>
-      <div className={Style['hero-content']} data-aos="fade-up">
-        <h1>مرحباً بكم في ترانسيسكو لوجستيك</h1>
-        <div className={Style.breadcrumb}>
-          <a href="#">الرئيسية</a>
-          <a href="#">عن الشركة</a>
+    <>
+      <Head>
+        <link
+          rel="preload"
+          as="image"
+          href="/img/Hero/inner-page1.webp"
+        />
+      </Head>
+
+      <div className={Style.hero}>
+        <div className={Style['hero-content']} data-aos="fade-up">
+          <h1>مرحباً بكم في ترانسيسكو لوجستيك</h1>
+          <div className={Style.breadcrumb}>
+            <a href="#">الرئيسية</a>
+            <a href="#">عن الشركة</a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
