920baea4e3f781f3f0bbae172176c609f81e5173
dhia hamedi <d.hamdi1710@myu.universitecentrale.tn^C>
2025-06-22 04:21:57 +0100

    test

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -4,12 +4,14 @@ import Card  from '../Components/Card/Card'
 import ScrollToTopButton from '../Components/ScrollToTopButton'
 import WhatsappButtons from '../Components/WhatsappButtons'
 import Banner from '../Components/Banner/Banner'
+import Banner2 from '../Components/Banner/Banner2'
 function index() {
   return (
     <div>
       <Hero/>
       <Card/>
       <Banner/>
+      <Banner2/>
       <ScrollToTopButton />
       <WhatsappButtons/>
     </div>
