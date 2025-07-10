af8de5f075b9a16d209a57ce108207a428244ee5
dhia hamedi <d.hamdi1710@myu.universitecentrale.tn^C>
2025-07-10 15:41:05 +0100

    verification pour le deployement

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,10 @@
-// lib/db.ts
 import mysql from 'mysql2/promise';
 
-const dbUrl = process.env.DATABASE_URL!;
-const url = new URL(dbUrl);
-
 const pool = mysql.createPool({
-  host: url.hostname,
-  user: url.username,
-  password: url.password,
-  database: url.pathname.replace('/', ''), // remove leading slash
-  port: Number(url.port) || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export async function query<T>(sql: string, params?: any[]): Promise<T[]> {
