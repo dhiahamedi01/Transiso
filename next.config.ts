6a081a0b66b310beb06ad48d270240df01d7f236
dhia hamedi <d.hamdi1710@myu.universitecentrale.tn^C>
2025-07-19 08:31:14 +0100

    Panier

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,11 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Images – tu gardes ton réglage
   images: {
     unoptimized: true,
   },
-
-  // ────────────────────────────────
-  // Internationalisation native Next.js
-  // ────────────────────────────────
   i18n: {
-    // ⚠️  liste EXACTEMENT les dossiers que tu as créés dans /public/locales
     locales: ['en', 'tr', 'ar'],
     defaultLocale: 'en',
-
-    // (optionnel) empê­che la redirection auto selon l’Accept‑Language
-    // localeDetection: false,
   },
 };
 
