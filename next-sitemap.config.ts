5d4aa97d1d292af54442bfcc0dbf99deabb8ec18
dhia hamedi <d.hamdi1710@myu.universitecentrale.tn^C>
2025-07-19 08:43:20 +0100

    Panier

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,7 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.transisologistic.com',
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-  };
-  
+  siteUrl: 'https://www.transisologistic.com',
+  generateRobotsTxt: true,
+};
